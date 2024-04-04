30130393977a8d757f959501ac4f07b342438140
eldor2003 <qulsoatoveldor@gmail.com>
2024-04-04 14:33:18 +0500

    font

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,32 +1,35 @@
-import {
-    AppBar, Container,
-    Toolbar,
-} from '@mui/material'
-import Logo from './Logo.jsx';
-import CentralButtons from './CentralButtons.jsx';
-import RightButtons from './RightButtons.jsx';
-import euIcon from '../../svg/EU.svg';
+import { AppBar, Container, Toolbar } from "@mui/material";
+import Logo from "./Logo.jsx";
+import CentralButtons from "./CentralButtons.jsx";
+import RightButtons from "./RightButtons.jsx";
+// import euIcon from '../../svg/EU.svg';
 
 const Header = () => {
-    return (
-        <AppBar
-            elevation={0}
-            sx={{
-                backgroundColor: "inherit",
-                position: "relative",
-                display: "flex",
-            }}
-        >
-            <Container className={'header'} sx={{borderRadius: "5px", backgroundColor: "rgba(26, 29, 37, 0.51)", marginTop: 3}}>
-                <Toolbar sx={{ justifyContent: "space-between" }}>
-                    <Logo />
-                    <CentralButtons />
-                    <RightButtons />
-                </Toolbar>
-            </Container>
-        </AppBar>
-    );
-}
+	return (
+		<AppBar
+			elevation={0}
+			sx={{
+				backgroundColor: "inherit",
+				position: "relative",
+				display: "flex",
+			}}
+		>
+			<Container
+				className={"header"}
+				sx={{
+					borderRadius: "5px",
+					backgroundColor: "rgba(26, 29, 37, 0.51)",
+					marginTop: 3,
+				}}
+			>
+				<Toolbar sx={{ justifyContent: "space-between" }}>
+					<Logo />
+					<CentralButtons />
+					<RightButtons />
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
+};
 
-
-export default Header
+export default Header;
